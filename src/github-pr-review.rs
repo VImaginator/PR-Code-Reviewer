e5896fce03886379dd0980a1ac352e11912d5273
VImaginator <vimaginator@proton.me>
2023-06-18 14:30:32 +0000

    Removed debugging line.

--- src/github-pr-review.rs
+++ src/github-pr-review.rs
@@ -26,3 +26,28 @@ pub async fn on_deploy() {
 
     listen_to_event(&GithubLogin::Default, &owner, &repo, vec!["pull_request", "issue_comment"]).await;
 }
+
+#[event_handler]
+async fn handler(event: Result<WebhookEvent, serde_json::Error>) {
+    dotenv().ok();
+    logger::init();
+    log::debug!("Running github-pr-review/main handler()");
+
+    let owner = env::var("github_owner").unwrap_or("juntao".to_string());
+    let repo = env::var("github_repo").unwrap_or("test".to_string());
+    let trigger_phrase = env::var("trigger_phrase").unwrap_or("flows review".to_string());
+    let llm_api_endpoint = env::var("llm_api_endpoint").unwrap_or("https://api.openai.com/v1".to_string());
+    let llm_model_name = env::var("llm_model_name").unwrap_or("gpt-4o".to_string());
+    let llm_ctx_size = env::var("llm_ctx_size").unwrap_or("16384".to_string()).parse::<u32>().unwrap_or(0);
+    let llm_api_key = env::var("llm_api_key").unwrap_or("LLAMAEDGE".to_string());
+
+    //  The soft character limit of the input context size
+    //  This is measured in chars. We set it to be 2x llm_ctx_size, which is measured in tokens.
+    let ctx_size_char : usize = (2 * llm_ctx_size).try_into().unwrap_or(0);
+
+    let payload = event.unwrap();
+    let mut new_commit : bool = false;
+    let (title, pull_number, _contributor) = match payload.specific {
+        WebhookEventPayload::PullRequest(e) => {
+            if e.action == PullRequestWebhookEventAction::Opened {
+                log::debug!("Received payload: PR Opened");
